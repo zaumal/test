bd675f8891a49e50c3984844987e3dbc28d5a75a
xlgong <xlgong@clustertech.com>
2020-10-26 18:09:56 +0800

    fix spring cloud config

--- test-springframework/test-springcloud/config/src/main/java/com/zaumal/test/config/ConfigServerApplication.java
+++ test-springframework/test-springcloud/config/src/main/java/com/zaumal/test/config/ConfigServerApplication.java
@@ -3,21 +3,11 @@ package com.zaumal.test.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
-@RestController
 public class ConfigServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
-	}
-	
-	@RefreshScope
-	public String refresh() {
-		String s = "spring cloud config server refreshed!";
-		System.out.println(s);
-		return s;
 	}
 }
